f693b40c647ef06eeeeeb57298abf3afe6a30d3e
djarae <djaraestivill@gmail.com>
2024-06-03 00:06:33 -0400

    feat: 1 Se crea console log base
    Se crea un console log base siguientes pasos:
    * Integrar simple con front
    * Insertar en BD

--- README.md
+++
@@ -1,2 +0,0 @@
-# projecto_smash_back
-Backend en springboot, para aplicacion front en Angular de data smash
--- smushBackend/src/main/java/com/example/demo/SmushBackendApplication.java
+++ smushBackend/src/main/java/com/example/demo/SmushBackendApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectoSmashBackApplication {
+public class SmushBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectoSmashBackApplication.class, args);
+		SpringApplication.run(SmushBackendApplication.class, args);
+		
+
+		System.out.println("Hola squirtle");
 	}
 
 }
